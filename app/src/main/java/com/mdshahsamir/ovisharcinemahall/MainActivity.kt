454a0d714aba80247c41e0e645c8d990eb20b0b9
shahsamir <mdshahsamir@gmail.com>
2023-08-10 12:44:39 +0600

    Create MainActivity NavHostFragment

--- app/src/main/java/com/mdshahsamir/ovisharcinemahall/MainActivity.kt
+++ app/src/main/java/com/mdshahsamir/ovisharcinemahall/MainActivity.kt
@@ -1,11 +1,25 @@
 package com.mdshahsamir.ovisharcinemahall
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupActionBarWithNavController
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val navHostFragment: NavHostFragment = supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
+        navController = navHostFragment.navController
+        setupActionBarWithNavController(navController)
+    }
+
+    override fun onNavigateUp(): Boolean {
+        return navController.navigateUp() || super.onNavigateUp()
     }
 }
